ce2b43e80ff2fb6c7a1692d2a90cb569fae93b72
Alex Master <mastrukov@yahoo.ca>
2020-04-11 08:55:48 -0400

    Add project files.

---
+++ SpotsStore.WebUI/Global.asax.cs
@@ -0,0 +1,17 @@
+using SportsStore.WebUI;
+using SportsStore.WebUI.Infrastructure;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SpotsStore.WebUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+        }   
+    }
+}
