a011dae44b93d8b2b7e75d3368a89e59e8df874b
Alex Master <mastrukov@yahoo.ca>
2020-04-12 08:46:51 -0400

    Added Admin Controller new methotds

--- SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -1,20 +1,49 @@
-﻿using SportsStore.Domain.Abstract;
+﻿using System;
+using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SportsStore.Domain.Concrete
 {
     public class EFProductRepository : IProductRepository
     {
+
         private EFDbContext context = new EFDbContext();
+
         public IQueryable<Product> Products
         {
             get { return context.Products; }
         }
 
+        public void DeleteProduct(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+        }
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                //  context.Entry(product).State = System.Data.EntityState.Modified;
+
+
+                Product dbEntry = context.Products.Find(product.ProductID);
+                if(dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+
+            context.SaveChanges();
+        }
     }
 }
+
